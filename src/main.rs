62753d5e66c137c2ffd7ec379352afc6014a04ca
Kriptikz <kriptikz@github.com>
2024-02-11 05:58:39 +0000

    feature: buttons stop/start log stream

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::{convert::Infallible, path::Path, time::Duration};
+use std::{
+    convert::Infallible,
+    path::Path,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use askama::Template;
 use axum::{
     http::{Response, StatusCode},
     response::{sse::Event, IntoResponse, Sse},
-    routing::get,
+    routing::{get, put},
     Extension, Router,
 };
 use logwatcher::LogWatcher;
@@ -16,16 +21,27 @@ use tokio_stream::{wrappers::BroadcastStream, Stream, StreamExt as _};
 
 pub type LogStream = Sender<String>;
 
+struct AppState {
+    stream_logs_toggle: bool,
+}
+
 #[tokio::main]
 async fn main() {
     let (tx, _rx) = channel::<String>(10);
+
+    let shared_state = Arc::new(Mutex::new(AppState {
+        stream_logs_toggle: false,
+    }));
 
     let app = Router::new()
         .route("/", get(index))
         .route("/styles.css", get(styles))
         .route("/script.js", get(script))
+        .route("/start-log-stream", put(handle_start_log_stream))
+        .route("/stop-log-stream", put(handle_stop_log_stream))
         .route("/stream-logs", get(handle_stream_logs))
-        .layer(Extension(tx.clone()));
+        .layer(Extension(tx.clone()))
+        .layer(Extension(shared_state));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listener bound to port 3000");
@@ -85,13 +101,30 @@ async fn styles() -> impl IntoResponse {
 async fn script() -> impl IntoResponse {
     Response::builder()
         .status(StatusCode::OK)
-        .header("Content-Type", "text/css")
+        .header("Content-Type", "text/javascript")
         .body(include_str!("../templates/script.js").to_owned())
         .unwrap()
 }
 
+async fn handle_start_log_stream(
+    Extension(shared_state): Extension<Arc<Mutex<AppState>>>,
+) -> impl IntoResponse {
+    let mut shared_state_lock = shared_state.lock().unwrap();
+    shared_state_lock.stream_logs_toggle = true;
+    StatusCode::OK
+}
+
+async fn handle_stop_log_stream(
+    Extension(shared_state): Extension<Arc<Mutex<AppState>>>,
+) -> impl IntoResponse {
+    let mut shared_state_lock = shared_state.lock().unwrap();
+    shared_state_lock.stream_logs_toggle = false;
+    StatusCode::OK
+}
+
 async fn handle_stream_logs(
     Extension(tx): Extension<LogStream>,
+    Extension(shared_state): Extension<Arc<Mutex<AppState>>>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let rx = tx.subscribe();
 
@@ -99,10 +132,15 @@ async fn handle_stream_logs(
 
     Sse::new(
         stream
-            .map(|msg| {
-                let msg = msg.unwrap();
-                let event_data = format!("<div>{}</div>", msg);
-                Event::default().data(event_data)
+            .map(move |msg| {
+                let shared_state = shared_state.lock().unwrap();
+                if shared_state.stream_logs_toggle {
+                    let msg = msg.unwrap();
+                    let event_data = format!("<div>{}</div>", msg);
+                    Event::default().event("log-stream").data(event_data)
+                } else {
+                    Event::default()
+                }
             })
             .map(Ok),
     )
--- templates/script.js
+++ templates/script.js
@@ -3,21 +3,16 @@ function toggleAutoScroll() {
     var scrollableDiv = document.getElementById('log-container');
 
     if (checkbox.checked) {
-        // Scroll to the bottom
-        console.log("SCROLL CHECKED");
         scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
     }
-    // If the checkbox is unchecked, do nothing
 }
 
-// Optional: Automatically scroll to the bottom if the checkbox is initially checked
 window.onload = function() {
     if (document.getElementById('toggleScroll').checked) {
         toggleAutoScroll();
     }
 };
 
-// Optional: Keep scrolling to the bottom if new content is added and the checkbox is checked
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (document.getElementById('toggleScroll').checked) {
